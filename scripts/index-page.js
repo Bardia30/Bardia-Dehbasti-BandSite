4b13fd98491f527515d295c7938c5608e86f23e9
Bardia30 <bardiadehbasti@gmail.com>
2023-04-03 22:21:03 -0400

    added all comments to JS, added some mixins

--- scripts/index-page.js
+++ scripts/index-page.js
@@ -26,7 +26,7 @@ const  convertDate = d => {
 
 
 //a function that loops through each item in commentsArray
-// with .forEach() method and uses displayComment function 
+// with .forEach() method and uses displayComment function
 //to render the comments on DOM
 const renderCommentsSection = () => {
     commentsArray.forEach((comment) => {
@@ -36,48 +36,69 @@ const renderCommentsSection = () => {
 
 
 
+//finding the section with class convo and storing it as a variable with name sectionParent
+const sectionParent = document.querySelector(".convo");
 
 
+//a function that takes in an object as argument
+// and creates displays a single comment block on the webpage
 const displayComment = (commentObject) => {
-    let name = commentObject.name;
+    //storing the name, commentText and timestamp of each object from the commentsArray
+    let name = commentObject.name; 
     let comment = commentObject.commentText;
-    let date = convertDate(commentObject.timestamp);
+    let date = convertDate(commentObject.timestamp); //converting the timestamp to m/dd/yyyy format
 
+    //create a new parent div element called newParentDiv 
+    //with class previous-comment and appending it to the sectionParent
     const newParentDiv = document.createElement("div");
     newParentDiv.classList.add("previous-comment");
     sectionParent.appendChild(newParentDiv);
     
-
+    //create a new parent div element called profilePic
+    //with class previous-comment__pic-placeholder 
+    //and appending it to the newParentDiv
+    //note: this is the gray profile pic 
     const profilePicDiv = document.createElement("div");
     profilePicDiv.classList.add("previous-comment__pic-placeholder");
     newParentDiv.appendChild(profilePicDiv);
     
 
+    //another parent div is created do enclose all the texts as children
+    //it is appended to newParentDiv (under profilePic)
     const textParent = document.createElement("div");
     textParent.classList.add("previous-comment__texts");
     newParentDiv.appendChild(textParent);
 
+
+
+    //a child div is created do enclose name and date as children
+    //and it is added to textParent
     const textTopDiv = document.createElement("div");
     textTopDiv.classList.add("previous-comment__texts-top");
     textParent.appendChild(textTopDiv);
 
+
+    //a paragraph element is created to enclose the commentText and is added to textParent
     const longParagraph = document.createElement("p");
     longParagraph.classList.add("previous-comment__long-paragraph");
-    longParagraph.innerText = comment
+    longParagraph.innerText = comment //innerText value is taken dynamically from the current object's property commentText
     textParent.appendChild(longParagraph);
-
+    
+    
+    //a paragraph element is created to enclose the name and is added to textTopDiv
     const nameUser = document.createElement("p");
     nameUser.classList.add("previous-comment__name");
-    nameUser.innerText = name;
+    nameUser.innerText = name; //innerText value is taken dynamically from the current object's property name
     textTopDiv.appendChild(nameUser);
 
+    //a paragraph element is created to enclose the date and is added to textTopDiv
     const dateComment = document.createElement("p");
     dateComment.classList.add("previous-comment__date");
-    dateComment.innerText = date;
+    dateComment.innerText = date; //innerText value is taken dynamically from the current object's property timestamp
     textTopDiv.appendChild(dateComment);
 
 
-    //insert horizantal rule
+    //a horizantal rule element is created and added beneath the previous-comment div
     const horRule = document.createElement("hr");
     horRule.classList.add("convo-divider");
     sectionParent.appendChild(horRule);
@@ -85,10 +106,13 @@ const displayComment = (commentObject) => {
 
 
 
-
+//a function to clear all the previous-comments divs, and all the horizantal rules that where added
 const clearCommentsSection = () => {
+    //finds an array of all elements with "previous-comment" class and store it as an variable name allCommentsDivs
     const allCommentDivs = sectionParent.querySelectorAll(".previous-comment");
+    //finds an array of all elements with "convo-divider" class and store it as an variable name allCommentsDivs
     const allHorRules = sectionParent.querySelectorAll(".convo-divider");
+    //loop through the array created above and delete all comments and hr elements
     for (i = 0; i < allCommentDivs.length;i++) {
         allCommentDivs[i].remove();
         allHorRules[i].remove();
@@ -97,45 +121,66 @@ const clearCommentsSection = () => {
 
 
 
-const sectionParent = document.querySelector(".convo");
-//adding comments to commentsArray
 
+//adding new comments to commentsArray
+
+
+
+
+//finding the form, and the button and storing them as variables
 const form = document.querySelector(".comment__form");
 const button = document.querySelector(".comment__btn");
 
 
 
-
+//we call renderCommentsSection in order to render the 
+//three default comments as required upon page reload
 renderCommentsSection();
 
+
+
+//a function that returns today's date in numeric format
 const generateTodayDate = () => {
     const todayDate = new Date();
     return todayDate[Symbol.toPrimitive]('number');
 }
 
 
+//adding an event listener to the form which is fired by the submit event
+//i.e. the anonymous callback funciton is triggered when the submit button is clicked
 form.addEventListener('submit', (e) => {
-    e.preventDefault();
+    e.preventDefault(); // to prevent the page reload when button is clicked
     
+    //storing what the user has typed in the name input and <textarea> (userComment) input
+    // as userName and userComment variables
     let userName = form.elements.userName.value;
     let userComments = form.elements.userComments.value
     
     
-
+    //call generateTodayDate() function and assign it to variable dateOfComment
+    //this will be the date that the user has entered the comment
     let dateOfComment = generateTodayDate();
 
+
+    //create a new empty obect named newCommentObject with properties name, commentText, ands timestamp
     let newCommentObject = {};
+    //the name property value is equal to the value entered by the user in the Name field
     newCommentObject.name = userName;
+    //the commentText property value is equal to userComments entered by the user
     newCommentObject.commentText = userComments;
+    //the timestamp property value is equal to dateOfComment which is the today's date converted from numeric format to m/dd/yyyy format
     newCommentObject.timestamp = dateOfComment;
     commentsArray.unshift(newCommentObject);
 
-
-    clearCommentsSection(); //fix these 
+    //we call clearCommentsSection function to clear all the previously rendered functions 
+    //and then call renderCommentsSection function to render the commentsArray which 
+    //now includes the newly added comment object entered by the user.  
+    clearCommentsSection(); 
     renderCommentsSection();
 
     
-
+    //we use the form method .reset() in order to bring back the values of 
+    //input and textarea elements back to default
     form.reset();
 });
 
